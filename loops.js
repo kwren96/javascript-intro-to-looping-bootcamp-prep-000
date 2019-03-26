a5f7a135df9d3a821b8a57f7ccf66966cf1ba94b
Katherine Wren <kwren@outlook.com>
2019-03-26 21:28:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,6 @@ function forLoop(array)
 {for (let i = 0; i < 25; i++) {if (i === 1) {array.push('I am 1 strange loop.')} else {array.push(`I am ${i} strange loops.`)}} return array}
 function whileLoop(n)
 {while (n>0) {console.log(--n);} return 'done'}
-var i = 0;
 function incrementVariable() {
   i = i + 1;
   return i;
